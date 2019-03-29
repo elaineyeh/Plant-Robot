e977608d738cce0d2d0bc4a9a5306e0d75a46ca0
elaineyeh <10456018@ntub.edu.tw>
2019-03-29 14:59:49 +0800

    delete the old version function

--- firebase_get.py
+++ firebase_get.py
@@ -9,10 +9,3 @@ firebase_admin.initialize_app(cred, {
 
 ref = db.reference('/')
 print(ref.get())
-
-#from firebase import firebase
-#
-#url = "https://plant-robot.firebaseio.com/"
-#fb = firebase.FirebaseApplication(url, None)
-#plants = fb.get("/plant-robot", None)
-#print(plants)
